93a9ab3aced79ba8c281ed3b1e35741b2634c17a
size1995 <35724004+size1995@users.noreply.github.com>
2019-05-31 17:02:12 +0800

    Add files via upload

---
+++ 用两个栈实现队列.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+    def push(self, node):
+        self.stack1.append(node)
+    def pop(self):
+        if self.stack2:
+            return self.stack2.pop()
+        elif not self.stack1:
+            return None
+        else:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+            return self.stack2.pop()
