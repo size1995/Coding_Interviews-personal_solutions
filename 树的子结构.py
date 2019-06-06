280cd8aaa11c7ab8bcabade67a89130969e7a451
size1995 <35724004+size1995@users.noreply.github.com>
2019-06-06 16:51:17 +0800

    Add files via upload

---
+++ 树的子结构.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def HasSubtree(self, pRoot1, pRoot2):
+        if not (pRoot1 and pRoot2):
+            return False
+        return self.hassub(pRoot1, pRoot2) or self.HasSubtree(pRoot1.left, pRoot2) or self.HasSubtree(pRoot1.right,
+                                                                                                      pRoot2)
+
+    def hassub(self, A, B):
+        if not B:
+            return True
+        if A == None or A.val != B.val:
+            return False
+        return self.hassub(A.left, B.left) and self.hassub(A.right, B.right)
