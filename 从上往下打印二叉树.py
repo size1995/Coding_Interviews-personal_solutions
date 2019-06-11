07013a2ecb813c16cd674e9878f5112c5d1ae4ef
size1995 <35724004+size1995@users.noreply.github.com>
2019-06-11 21:50:51 +0800

    Add files via upload

---
+++ 从上往下打印二叉树.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    def PrintFromTopToBottom(self, root):
+        result=[]
+        if not root:
+            return result
+        queue=[root]
+        while queue:
+            current_node=queue.pop(0)
+            result.append(current_node.val)
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+        return result
